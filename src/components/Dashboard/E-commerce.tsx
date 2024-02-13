b825fdb47b1d3cdf4d58f7c738bb84d38c1bbabe
ines <inesmrad20@gmail.com>
2024-02-13 13:20:51 +0100

    "+"

--- src/components/Dashboard/E-commerce.tsx
+++ src/components/Dashboard/E-commerce.tsx
@@ -2,13 +2,12 @@
 import React from "react";
 import ChartOne from "../Charts/ChartOne";
 import ChartThree from "../Charts/ChartThree";
-import { Link, useLocation } from "react-router-dom";
 import TableOne from "../Tables/TableOne";
 import CardDataStats from "../CardDataStats";
 import MapOne from "../Maps/MapOne";
 
 const ECommerce: React.FC = () => {
-  const { pathname } = useLocation();
+  
   return (
     <>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
