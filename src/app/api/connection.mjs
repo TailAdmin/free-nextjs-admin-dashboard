a1e8d632e7595851e6f959079f15bee22aa29c92
Lina Fadhli <133816965+luluna02@users.noreply.github.com>
2024-02-16 11:16:54 +0100

    lina last changes

--- src/app/api/connection.mjs
+++ src/app/api/connection.mjs
@@ -1,28 +1,3 @@
-/*import mysql from 'mysql2/promise';
-
-export default async function handler(
-  req,
-  res
-) {
-
-  const connection = await mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: '98215062Mlina',
-    database: 'fursaDash',
-  });
-  
-  try {
-    
-    const [rows] = await connection.execute('SELECT * FROM users');
-    await connection.end();
-
-    res.status(200).json(rows);
-  } catch (error) {
-    console.error('API error:', error);
-    res.status(500).json({ message: 'Server error' });
-  }
-}*/
 import cors from 'cors';
 import express from 'express';
 import getUsers from './db.mjs'
--- src/app/api/db.mjs
+++ src/app/api/db.mjs
@@ -1,27 +1,3 @@
-/*import mysql from 'mysql2/promise';
-
-export default async function handler(
-  req,
-  res
-) {
-
-  const connection = await mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '98215062Mlina',
-    database: 'fursaDash',
-  });
-  
-  try {
-    const [rows] = await connection.execute('SELECT * FROM users');
-    await connection.end();
-    res.status(200).json({rows});
-    console.log({rows});
-  } catch (error) {
-    console.error('API error:', error);
-    res.status(500).json({ message: 'Server error' });
-  }
-}*/
 
 import mysql from 'mysql2';
 
@@ -37,4 +13,5 @@ export default async function getUsers() {
   return rows
 }
 
-const users = await getUsers()
+
+const users = await getUsers();
