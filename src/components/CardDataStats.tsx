99a9380c3439d9ef149db5026c6683584d7d1d9f
nehemiahcode <codehubby@gmail.com>
2024-03-27 22:30:08 +0100

    first commit

--- src/components/CardDataStats.tsx
+++ src/components/CardDataStats.tsx
@@ -18,7 +18,7 @@ const CardDataStats: React.FC<CardDataStatsProps> = ({
   children,
 }) => {
   return (
-    <div className="rounded-sm border border-stroke bg-white px-7.5 py-6 shadow-default dark:border-strokedark dark:bg-boxdark">
+    <div className="rounded-sm border border-stroke bg-white px-7.5 py-7 shadow-default dark:border-strokedark dark:bg-boxdark">
       <div className="flex h-11.5 w-11.5 items-center justify-center rounded-full bg-meta-2 dark:bg-meta-4">
         {children}
       </div>
