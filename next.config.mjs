a0f979944b2440033f33145c392451b61a485b4a
Andres Vallecilla <andresfelipe083195@hotmail.com>
2024-06-11 11:34:02 -0500

    build: add variables enviroment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+  swcMinify: true,
+  env: {
+    BACKEND_BASE_PATH: process.env.BACKEND_BASE_PATH,
+    KEYCLOAK_URL: process.env.KEYCLOAK_URL,
+    KEYCLOAK_CLIENT_ID: process.env.KEYCLOAK_CLIENT_ID,
+    KEYCLOAK_REDIRECT_URI: process.env.KEYCLOAK_REDIRECT_URI,
+    KEYCLOAK_POST_LOGOUT_REDIRECT_URI: process.env.KEYCLOAK_POST_LOGOUT_REDIRECT_URI,
+  }
+};
 
 export default nextConfig;
