d89b4440300f6bb5ff7d8518ad32b8d04ab3e975
Pushpal Ghoshal <pushpql.ghoshal@gmail.com>
2024-06-17 23:07:51 +0530

    fix: remove dist folder in put
    - according to jsvectormap docs, this is no longer required in the latest update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 "use client";
-import "jsvectormap/dist/css/jsvectormap.css";
+import "jsvectormap/dist/jsvectormap.css";
 import "flatpickr/dist/flatpickr.min.css";
 import "@/css/satoshi.css";
 import "@/css/style.css";
