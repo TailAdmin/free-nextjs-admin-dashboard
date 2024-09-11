db2495ce71ca54316b08e391fad73bfde62ecf7e
olegtetenev <otetenev@gmail.com>
2024-09-11 17:54:09 +0300

    Fix case issue for Loader component

--- src/components/common/Loader/index.tsx
+++ src/components/common/Loader/index.tsx
@@ -3,9 +3,7 @@ import { Spinner } from "@nextui-org/react";
 const Loader = () => {
   return (
     <div className="flex h-screen items-center justify-center bg-white dark:bg-black">
-      {/* <div className="h-16 w-16 animate-spin rounded-full border-4 border-solid border-primary border-t-transparent">
 
-      </div> */}
       <Spinner size="lg" />
     </div>
   );
