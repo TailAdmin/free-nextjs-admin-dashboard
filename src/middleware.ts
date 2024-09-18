35a4cfac431592bf4eded5fd2714aeb5420305a1
olegtetenev <otetenev@gmail.com>
2024-09-18 10:11:12 +0300

    bugfix: middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -20,8 +20,11 @@ export async function middleware(req: NextRequest) {
     const currentIp = forwarded ? forwarded.split(',')[0].trim() : req.ip;
 
     
-    const allowedIps = process.env. AGHANIM_ALLOWED_IPS;
-    if (currentIp && currentIp !== '::1' && allowedIps && allowedIps.length > 0) {
+    const allowedIps = process.env.AGHANIM_ALLOWED_IPS ? JSON.parse(process.env.AGHANIM_ALLOWED_IPS) : [];
+
+    if (currentIp &&
+        //currentIp && currentIp !== '::1' && 
+        allowedIps && allowedIps.length > 0) {
         if (!allowedIps.includes(currentIp)){
             logger.warn(`User ${token?.name} with IP ${currentIp} is not allowed. Request rejected.`);
             return NextResponse.json({ message: 'IP is not allowed' }, { status: 403 });
