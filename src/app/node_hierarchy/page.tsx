af0f98f45020dc9d09f795363ee8b3c16b81f264
DerrickXu1998 <dyuxu01@gmail.com>
2024-12-07 23:48:28 +0000

    fix

--- src/app/node_hierarchy/page.tsx
+++ src/app/node_hierarchy/page.tsx
@@ -15,21 +15,7 @@ const Profile = () => {
       <div className="mx-auto max-w-242.5">
         <Breadcrumb pageName="Node Hierarchy" />
         {/* hooking up sending to svc */}
-        <div className="relative z-20 h-10 md:h-10"></div>
         <LoadGraphWithHook style={{ height: "400px" }} />
-        <div
-          id="FileUpload"
-          className="relative mb-5.5 block w-full cursor-pointer appearance-none rounded border border-dashed border-primary bg-gray px-4 py-4 dark:bg-meta-4 sm:py-7.5"
-        >
-          <input
-            type="file"
-            accept="image/*"
-            className="absolute inset-0 z-50 m-0 h-full w-full cursor-pointer p-0 opacity-0 outline-none"
-          />
-          <div id="container" className="flex flex-col items-center justify-center space-y-3">
-            
-          </div>
-        </div>
       </div>
     </DefaultLayout>
   );
