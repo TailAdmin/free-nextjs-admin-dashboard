fc2d9715c1a4c352deffe87b9ec5431bd301474f
SharveshC <sharvesh1817@gmail.com>
2026-01-05 22:13:21 +0530

    auto: changes @ 2026-01-05 22:13:21

---
+++ src/data/users.ts
@@ -0,0 +1,16 @@
+export const users = [
+  {
+    id: "1",
+    name: "Admin User",
+    email: "admin@example.com",
+    role: "Admin",
+    status: "Active",
+  },
+  {
+    id: "2",
+    name: "Regular User",
+    email: "user@example.com",
+    role: "User",
+    status: "Inactive",
+  },
+];
