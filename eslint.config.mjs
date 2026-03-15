7394e1bada75a2eabf31699512a68755805948b1
Naim Uddin <naim.pimjo@gmail.com>
2026-03-15 10:27:30 +0600

    update ESLint configuration and dependencies; upgrade Next.js to version 16.1.6 and adjust TypeScript JSX setting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,18 @@
-// eslint.config.mjs
-import { defineConfig, globalIgnores } from 'eslint/config'
-import nextConfig from 'eslint-config-next/core-web-vitals'
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from "eslint/config";
 
-export default defineConfig([
-  ...nextConfig,
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  // Override default ignores of eslint-config-next.
   globalIgnores([
-    '.next/**',
-    'out/**',
-    'build/**',
-    'next-env.d.ts'
+    // Default ignores of eslint-config-next:
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
   ]),
-])
+]);
+
+export default eslintConfig;
