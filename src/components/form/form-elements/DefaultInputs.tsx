eaa0681bd39ff04e82857e9aa18998fe87024ab8
sinyen04 <yeesinyen13@gmail.com>
2026-04-15 13:06:22 +0800

    Updated componenetcard title to Basic Inputs

--- src/components/form/form-elements/DefaultInputs.tsx
+++ src/components/form/form-elements/DefaultInputs.tsx
@@ -22,7 +22,7 @@ export default function DefaultInputs() {
       <div className="space-y-6">
         <div>
           <Label>Input</Label>
-          <Input type="text" />
+          <Input type="text" placeholder="Enter here" />
         </div>
         <div>
           <Label>Input with Placeholder</Label>
@@ -32,13 +32,13 @@ export default function DefaultInputs() {
           <Label>Select Input</Label>
           <div className="relative">
             <Select
-            options={options}
-            placeholder="Select an option"
-            onChange={handleSelectChange}
-            className="dark:bg-dark-900"
-          />
-             <span className="absolute text-gray-500 -translate-y-1/2 pointer-events-none right-3 top-1/2 dark:text-gray-400">
-              <ChevronDownIcon/>
+              options={options}
+              placeholder="Select an option"
+              onChange={handleSelectChange}
+              className="dark:bg-dark-900"
+            />
+            <span className="absolute text-gray-500 -translate-y-1/2 pointer-events-none right-3 top-1/2 dark:text-gray-400">
+              <ChevronDownIcon />
             </span>
           </div>
         </div>
